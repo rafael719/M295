d600a952341db6f5d446136911c03096fe0fc794
rafael719 <rafael.stern@lernende.bbw.ch>
2025-05-26 09:43:15 +0200

    f

---
+++ Block 1/my-first-async-io.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  const lines = data.split('\n').length - 1;
+  console.log(lines);
+});
